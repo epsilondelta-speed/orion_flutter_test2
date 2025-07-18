29a5a5d6910be57569d3e7b0bcf8836682d29f0e
Prathamesh Rasam <25prathamesh@gmail.com>
2025-07-18 17:19:33 +0530

    android folder

---
+++ android/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "co.epsilondelta.orion_flutter"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+     flatDir {
+        dirs("libs") // 👈 Tells Gradle to look for .aar here
+    }
+}
+
+dependencies {
+   implementation(":orion_flutter-release@aar") // ✅ correct in .kts
+}
