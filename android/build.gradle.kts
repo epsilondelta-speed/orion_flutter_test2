e4376f007f4bdc92d6ba91e2004a8927cac802cc
Prathamesh Rasam <45089785+prasam25@users.noreply.github.com>
2025-07-19 11:48:43 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,26 @@ plugins {
     kotlin("android")
 }
 
+buildscript {
+    ext.kotlin_version = "1.8.22"
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 android {
     namespace = "co.epsilondelta.orion_flutter"
     compileSdk = 35
@@ -12,14 +32,30 @@ android {
         targetSdk = 34
     }
 
+buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
+ testOptions {
+        unitTests.all {
+            enabled = false
+        }
+    }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+dependencies {
+ implementation("com.squareup.okhttp3:okhttp:4.12.0")
+        implementation("org.json:json:20231013")
+}
+
 }
 
 repositories {
@@ -28,7 +64,4 @@ repositories {
 
 }
 
-dependencies {
- implementation("com.squareup.okhttp3:okhttp:4.12.0")
-        implementation("org.json:json:20231013")
-}
+
